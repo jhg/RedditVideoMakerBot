2ac6f7204c2d2fe3caa89806fa28c2e2cb10e67d
Jesus Hernandez <jesushdez@protonmail.com>
2025-07-05 18:42:42 +0200

    Fix a small problem to simplify all

--- run.sh
+++ run.sh
@@ -10,4 +10,5 @@ if [ ! -f "$CONFIG_FILE" ]; then
   cp "$TEMPLATE_FILE" "$CONFIG_FILE"
 fi
 
+docker rm rvmt
 docker run --name rvmt -v $SCRIPT_DIR/out/:/app/assets -v $SCRIPT_DIR/.env:/app/.env -v $SCRIPT_DIR/config.toml:/app/config.toml -it rvmt
