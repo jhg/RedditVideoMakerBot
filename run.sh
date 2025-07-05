30855e10bc6de86205732a18d71fa85844297b2f
Jesus Hernandez <jesushdez@protonmail.com>
2025-07-05 17:55:46 +0200

    Simplify usage in run script and add a prune script

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-docker run -v $(pwd)/out/:/app/assets -v $(pwd)/.env:/app/.env -it rvmt
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+CONFIG_FILE="$SCRIPT_DIR/config.toml"
+TEMPLATE_FILE="$SCRIPT_DIR/utils/.config.template.toml"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Copying config template..."
+  cp "$TEMPLATE_FILE" "$CONFIG_FILE"
+fi
+
+docker run --rm -v $SCRIPT_DIR/out/:/app/assets -v $SCRIPT_DIR/.env:/app/.env -v $SCRIPT_DIR/config.toml:/app/config.toml -it rvmt
